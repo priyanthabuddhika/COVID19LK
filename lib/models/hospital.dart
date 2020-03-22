8133cd46a0978f061282493492f2c35b388c8d22
priyanthabuddhika <priyanthabuddhika@gmail.com>
2020-03-22 13:56:54 +0530

    added hospital model class for json serialization

---
+++ lib/models/hospital.dart
@@ -0,0 +1,47 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'hospital.g.dart';
+
+@JsonSerializable()
+class Hospital {
+  @JsonKey(name: 'id')
+  int id;
+  @JsonKey(name: 'name')
+  String name;
+  @JsonKey(name: 'cumulative_local')
+  int cumulativeLocal;
+  @JsonKey(name: 'cumulative_foreign')
+  int cumulativeForeign;
+  @JsonKey(name: 'cumulative_total')
+  int cumulativeTotal;
+  @JsonKey(name: 'treatment_local')
+  int treatmentLocal;
+  @JsonKey(name: 'treatment_foreign')
+  int treatmentForeign;
+  @JsonKey(name: 'treatment_total')
+  int treatmentTotal;
+
+
+  Hospital(
+      {this.cumulativeLocal,
+      this.cumulativeForeign,
+      this.cumulativeTotal,
+      this.treatmentLocal,
+      this.treatmentForeign,
+      this.treatmentTotal});
+
+  factory Hospital.fromJson(Map<String, dynamic> json) => _$HospitalFromJson(json);
+  Map<String, dynamic> toJson() => _$HospitalToJson(this);
+}
+
+class HospitalList {
+  List<Hospital> hospitals;
+  HospitalList({this.hospitals});
+
+  factory HospitalList.fromJson(List<dynamic> json) {
+  return HospitalList(
+      hospitals: json
+          .map((e) => Hospital.fromJson(e as Map<String, dynamic>))
+          .toList());
+}
+}
